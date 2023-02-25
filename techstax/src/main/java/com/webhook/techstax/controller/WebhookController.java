9dd2817ab34640410e32e865d9614ea701f52dfa
gunalgunal <seelanguna530@gmail.com>
2023-02-25 10:48:03 +0530

    logger implemented

--- techstax/src/main/java/com/webhook/techstax/controller/WebhookController.java
+++ techstax/src/main/java/com/webhook/techstax/controller/WebhookController.java
@@ -1,5 +1,7 @@
 package com.webhook.techstax.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookController {
 
-
+   private Logger logger= LoggerFactory.getLogger(WebhookController.class);
     @PostMapping("/webhook")
     public ResponseEntity<String> getDataFromGithub(@RequestBody String data)
     {
-        System.out.println(data);
+        logger.info("webhook:"+data);
         return new ResponseEntity<String>(data, HttpStatus.OK);
     }
 }
