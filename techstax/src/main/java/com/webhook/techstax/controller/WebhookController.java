7732f786b32069f088540396e0b8e4307cf708c3
gunalgunal <seelanguna530@gmail.com>
2023-02-25 11:52:58 +0530

    logging file altered 3

--- techstax/src/main/java/com/webhook/techstax/controller/WebhookController.java
+++ techstax/src/main/java/com/webhook/techstax/controller/WebhookController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,12 @@ public class WebhookController {
     {
         logger.info("webhook:"+data);
         return new ResponseEntity<String>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        logger.info("hello world");
+        return "hello";
     }
 }
